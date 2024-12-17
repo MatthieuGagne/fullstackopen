bc3a2b51bba8683a34dca223e1e84c5238c5f9c3
Matthieu Gagné <matthieu.gagne@telus.com>
2024-12-17 16:10:31 -0500

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Phonebook from './components/Phonebook'
 import AddForm from './components/AddForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phoneNumber: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122', id: 4 }
-  ]) 
-  const [filterString, setFilterString] = useState('Ab')
+  const [persons, setPersons] = useState([]) 
+  const [filterString, setFilterString] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])  
 
   return (
     <div>
