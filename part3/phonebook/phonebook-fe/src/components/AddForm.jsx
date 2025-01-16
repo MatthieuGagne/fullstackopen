86d25e2fa6fcdae526b3113cc4aaa2015366592e
Matthieu Gagne <t817646@telus.onmicrosoft.com>
2025-01-16 15:29:12 -0500

    exercise 3.18

--- part3/phonebook/phonebook-fe/src/components/AddForm.jsx
+++ part3/phonebook/phonebook-fe/src/components/AddForm.jsx
@@ -7,56 +7,40 @@ const AddForm = ({ persons, setPersons, setErrorMessage, setSuccessMessage }) =>
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
   
-    const addPerson = (event) => {
-      event.preventDefault();
-      const personObject = {
-          name: newName,
-          number: newNumber
-      };
+    const addPerson = async (event) => {
+        event.preventDefault();
+        const personObject = {
+            name: newName,
+            number: newNumber
+        };
 
-      const existingPerson = persons.find(person => person.name === personObject.name);
+        const existingPerson = persons.find(person => person.name === personObject.name);
 
-      if (existingPerson) {
-          if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)) {
-              personService
-                  .update(existingPerson.id, { ...existingPerson, number: newNumber })
-                  .then(updatedPerson => {
-                      setPersons(persons.map(person => person.id !== existingPerson.id ? person : updatedPerson));
-                      setNewName('');
-                      setNewPhoneNumber('');
-                      setSuccessMessage(`${newName} phone number was updated to ${newNumber}.`)
-                      setTimeout(() => {
-                        setSuccessMessage(null)
-                      }, 5000)
-                  })
-                  .catch(error => {
-                    setErrorMessage(`Problem while updating ${newName} phone number`);
-                    setTimeout(() => {
-                        setErrorMessage(null);
-                    }, 5000);
-                });                  
-          }
-      } else {
-          personService
-              .create(personObject)
-              .then(response => {
-                  setPersons(persons.concat(response));
-                  setNewName('');
-                  setNewNumber('');
-                  setSuccessMessage(`${newName} was added to the phonebook.`)
-                  setTimeout(() => {
-                    setSuccessMessage(null)
-                  }, 5000)
-                })
-                .catch(error => {
-                  setErrorMessage(`Problem while adding  ${newName} to the phonebook`);
-                  setTimeout(() => {
-                      setErrorMessage(null);
-                  }, 5000);
-              });                  
-
-      }
-  };
+        try {
+            if (existingPerson) {
+                if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)) {
+                    const updatedPerson = await personService.update(existingPerson.id, { ...existingPerson, number: newNumber });
+                    setPersons(persons.map(person => person.id !== existingPerson.id ? person : updatedPerson));
+                    setSuccessMessage(`${newName} phone number was updated to ${newNumber}.`);
+                }
+            } else {
+                const response = await personService.create(personObject);
+                setPersons(persons.concat(response));
+                setSuccessMessage(`${newName} was added to the phonebook.`);
+            }
+            setNewName('');
+            setNewNumber('');
+            setTimeout(() => {
+                setSuccessMessage(null);
+            }, 5000);
+        } catch (error) {
+            console.log(error);
+            setErrorMessage(`Problem while processing ${newName}`);
+            setTimeout(() => {
+                setErrorMessage(null);
+            }, 5000);
+        }
+    };
     
     const handlePersonChange = (event) => {
       console.log(`Person : ${event.target.value}`)
