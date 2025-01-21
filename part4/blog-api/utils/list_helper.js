7f51f8285ce72d61ff5a48259b69fe7a5d13c85c
Matthieu Gagne <matthieu.gagne@telus.com>
2025-01-21 14:36:10 -0500

    4.5

--- part4/blog-api/utils/list_helper.js
+++ part4/blog-api/utils/list_helper.js
@@ -7,7 +7,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  const { title, author, likes } = blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite, blogs[0])
+  return { title, author, likes }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
