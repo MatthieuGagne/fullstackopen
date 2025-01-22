d3c99762e82ce3eff24f5dd55fa77af89a106d0d
Matthieu Gagne <matthieu.gagne@telus.com>
2025-01-22 11:18:17 -0500

    4.7

--- part4/blog-api/utils/list_helper.js
+++ part4/blog-api/utils/list_helper.js
@@ -11,7 +11,7 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
-  if (blogs.length === 0) {
+  if (!blogs || blogs.length === 0) {
     return null
   }
   const { title, author, likes } = blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite, blogs[0])
@@ -30,9 +30,40 @@ const authorWithMostBlogs = (blogs) => {
   }
 }
 
+/**
+ * Returns the author with the most likes from a list of blogs.
+ *
+ * @param {Array} blogs - An array of blog objects.
+ * @returns {Object|null} An object with 'author' and 'likes' properties, or null if the input array is empty.
+ *
+ * The function works as follows:
+ * 1. It uses the reduce method to create an object (likesByAuthor) where the keys are author names and the values are the sum of likes for each author.
+ *    - The reduce method iterates over each blog in the blogs array.
+ *    - For each blog, it checks if the author already exists in the likesByAuthor object.
+ *    - If the author exists, it adds the current blog's likes to the existing value.
+ *    - If the author does not exist, it initializes the author's likes with the current blog's likes.
+ * 2. It finds the author with the maximum likes using the maxBy function.
+ * 3. It returns an object containing the author with the most likes and the total number of likes.
+ */
+const authorWithMostLikes = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return null
+  }
+  const likesByAuthor = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes
+    return acc
+  }, {})
+  const maxAuthor = maxBy(Object.keys(likesByAuthor), (author) => likesByAuthor[author])
+  return {
+    author: maxAuthor,
+    likes: likesByAuthor[maxAuthor]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  authorWithMostBlogs
+  authorWithMostBlogs,
+  authorWithMostLikes
 }
