2591789b6efc8963ea95d726a841bd0cd38bc384
Matthieu Gagne <matthieu.gagne@telus.com>
2025-02-07 13:38:02 -0500

    Exercises 4.11/4.12

--- part4/blog-api/tests/part4b.test.js
+++ part4/blog-api/tests/part4b.test.js
@@ -46,6 +46,48 @@ test('a valid blog can be added', async () => {
   assert(titles.includes('Test Blog'))
 })
 
+test('if likes property is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'Test Blog Without Likes',
+    author: 'Test Author',
+    url: 'http://testblog.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+test('backend responds with 400 Bad Request if title is missing', async () => {
+  const newBlog = {
+    author: 'Test Author',
+    url: 'http://testblog.com',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+test('backend responds with 400 Bad Request if url is missing', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
